f7e1fd862d6aad2fe8003121fd174bd991109668
Arda Berktin <arberktin@gmail.com>
2023-05-03 17:10:47 -0700

    Initial Commit

---
+++ MountDoug Maps/MapView.swift
@@ -0,0 +1,47 @@
+//
+//  MapView.swift
+//  MountDoug Maps
+//
+//  Created by Arda Berktin on 2023-05-01.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    let number: Int
+    
+    var navText: String {
+            if number == 1 {
+                return "Map of the First Floor"
+            } else if number == 2 {
+                return "Map of the Second Floor"
+            } else {
+                return "Room: \(number)"
+            }
+        }
+    
+
+    var body: some View {
+        
+        VStack{
+            Image(String(number)) .resizable()
+                .scaledToFit()
+                .aspectRatio(contentMode: .fit)
+        }
+        .navigationBarTitle(navText)
+        .navigationBarTitleDisplayMode(.inline)
+        .foregroundColor(.white)
+        .background(.black)
+    .toolbarColorScheme(.dark, for: .navigationBar)
+    .toolbarBackground(.purple,for: .navigationBar)
+    .toolbarBackground(.visible, for: .navigationBar)
+        
+    }
+    
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView(number: 106)
+    }
+}
---
+++ MountDoug Maps/MountDoug_MapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MountDoug_MapsApp.swift
+//  MountDoug Maps
+//
+//  Created by Arda Berktin on 2023-05-01.
+//
+
+import SwiftUI
+
+@main
+struct MountDoug_MapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
